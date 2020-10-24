7a93d2c91b1c8d253f06aabf93bc20ed44529e46
Nu Uyen Thi PHAN <thiphan94@gmail.com>
2020-10-24 21:57:14 +0200

    hello

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.myapplication
 
-
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-
-
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        title = "Budget App"
+        val button:Button = findViewById(R.id.button1)
+        button.setOnClickListener {
+            val intent = Intent(this@MainActivity, NewActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/NewActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/NewActivity.kt
@@ -1,11 +1,18 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 class NewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
+        title = "Budget App"
+        val button: Button = findViewById(R.id.button2)
+        button.setOnClickListener {
+            val i = Intent(this@NewActivity, MainActivity::class.java)
+            startActivity(i)
+        }
     }
 }
