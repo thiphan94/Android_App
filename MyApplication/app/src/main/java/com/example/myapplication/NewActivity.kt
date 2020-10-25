520d4fab6c813325fa3517177786e93dd3b8b5cd
Nu Uyen Thi PHAN <thiphan94@gmail.com>
2020-10-25 17:53:51 +0100

    input

--- MyApplication/app/src/main/java/com/example/myapplication/NewActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/NewActivity.kt
@@ -4,6 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+
+
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+
+
 class NewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +23,23 @@ class NewActivity : AppCompatActivity() {
             val i = Intent(this@NewActivity, MainActivity::class.java)
             startActivity(i)
         }
+
+        val personNames = arrayOf("Cinema", "Comestique", "Cours", "School", "Hobbit", "Love")
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        if (spinner != null) {
+            val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, personNames)
+            spinner.adapter = arrayAdapter
+
+            spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    Toast.makeText(this@NewActivity, getString(R.string.selected_item) + " " + personNames[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // Code to perform some action when nothing is selected
+                }
+            }
+        }
+
     }
 }
--- TEST/app/src/main/java/com/example/test/MainActivity.kt
+++ TEST/app/src/main/java/com/example/test/MainActivity.kt
@@ -1,11 +1,33 @@
-package com.example.test
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
-class MainActivity : AppCompatActivity() {
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    lateinit var spinner: Spinner
+    lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "KotlinApp"
+        textView = findViewById(R.id.textView)
+        spinner = findViewById(R.id.spinner)
+        val adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.birds,
+                android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = this
+    }
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val text: String = parent?.getItemAtPosition(position).toString()
+        textView.text = text
     }
 }
