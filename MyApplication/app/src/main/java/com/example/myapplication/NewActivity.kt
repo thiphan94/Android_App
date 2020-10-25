7d0c8596953a64f6c3135e71e24245af7cc7e4a6
Nu Uyen Thi PHAN <thiphan94@gmail.com>
2020-10-25 19:04:06 +0100

    add

--- MyApplication/app/src/main/java/com/example/myapplication/NewActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/NewActivity.kt
@@ -2,18 +2,19 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
-
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.app.DatePickerDialog
+import android.widget.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class NewActivity : AppCompatActivity() {
+    var button_date: Button? = null
+    var textview_date: TextView? = null
+    var cal = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
@@ -39,6 +40,24 @@ class NewActivity : AppCompatActivity() {
                     // Code to perform some action when nothing is selected
                 }
             }
+        }
+
+        val mPickTimeBtn = findViewById<Button>(R.id.pickDateBtn)
+        val textView     = findViewById<TextView>(R.id.dateTv)
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+        mPickTimeBtn.setOnClickListener {
+
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                // Display Selected date in TextView
+                textView.setText("" + dayOfMonth + " " + month + ", " + year)
+            }, year, month, day)
+            dpd.show()
+
         }
 
     }
