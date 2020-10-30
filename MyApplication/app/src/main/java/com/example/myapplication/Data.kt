f707ff4763a95dc61d76666bdb8239abf11baf3c
Nu Uyen Thi PHAN <thiphan94@gmail.com>
2020-10-30 15:09:25 +0100

    activity 3

--- MyApplication/app/src/main/java/com/example/myapplication/Data.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Data.kt
@@ -1,11 +1,20 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Data : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data)
+
+        val button: Button = findViewById(R.id.button2)
+        button.setOnClickListener {
+            val i = Intent(this@Data, NewActivity::class.java)
+            startActivity(i)
+        }
     }
+
 }
--- MyApplication/app/src/main/java/com/example/myapplication/NewActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/NewActivity.kt
@@ -19,21 +19,28 @@ class NewActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
         title = "Budget App"
+
+        val set: Button = findViewById(R.id.button)
+        set.setOnClickListener {
+            val i = Intent(this@NewActivity, Data::class.java)
+            startActivity(i)
+        }
+        
         val button: Button = findViewById(R.id.button2)
         button.setOnClickListener {
             val i = Intent(this@NewActivity, MainActivity::class.java)
             startActivity(i)
         }
 
-        val personNames = arrayOf("Cinema", "Comestique", "Cours", "School", "Hobbit", "Love")
+        val categories = arrayOf("Cinema", "Comestique", "Cours", "School", "Hobbit", "Love")
         val spinner = findViewById<Spinner>(R.id.spinner)
         if (spinner != null) {
-            val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, personNames)
+            val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
             spinner.adapter = arrayAdapter
 
             spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
-                    Toast.makeText(this@NewActivity, getString(R.string.selected_item) + " " + personNames[position], Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@NewActivity, getString(R.string.selected_item) + " " + categories[position], Toast.LENGTH_SHORT).show()
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>) {
@@ -42,6 +49,22 @@ class NewActivity : AppCompatActivity() {
             }
         }
 
+        val type = arrayOf("Income", "Expense")
+        val spinner2 = findViewById<Spinner>(R.id.spinner2)
+        if (spinner2 != null) {
+            val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, type)
+            spinner2.adapter = arrayAdapter
+
+            spinner2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    Toast.makeText(this@NewActivity, getString(R.string.selected_item) + " " + type[position], Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // Code to perform some action when nothing is selected
+                }
+            }
+        }
         val mPickTimeBtn = findViewById<Button>(R.id.pickDateBtn)
         val textView     = findViewById<TextView>(R.id.dateTv)
 
