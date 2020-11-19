280ecba75a91eb79c64ffcfcd47909f36a36ac45
Nu Uyen Thi PHAN <thiphan94@gmail.com>
2020-11-19 17:51:42 +0100

    animation

--- Budget_App/app/src/main/java/com/example/budget_app/MainActivity.kt
+++ Budget_App/app/src/main/java/com/example/budget_app/MainActivity.kt
@@ -4,9 +4,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,5 +19,15 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, Login::class.java)
             startActivity(intent)
         }
+
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.top_animation)
+        val icon: ImageView = findViewById(R.id.imageView)
+        icon.animation = animation;
+
+        val animation_button = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
+        val button2: Button = findViewById(R.id.button)
+        button2.animation = animation_button;
     }
 }
+
