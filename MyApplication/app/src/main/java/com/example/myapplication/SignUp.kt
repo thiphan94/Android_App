88ba2d92f7bb44749a4c818fceb03a9529212036
Nu Uyen Thi PHAN <thiphan94@gmail.com>
2020-12-11 00:18:26 +0100

    firebase signup

--- MyApplication/app/src/main/java/com/example/myapplication/SignUp.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/SignUp.kt
@@ -3,7 +3,12 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +21,41 @@ class SignUp : AppCompatActivity() {
             val intent = Intent(this@SignUp, Login::class.java)
             startActivity(intent)
         }
+
+        val button2: Button = findViewById(R.id.button3)
+        button2.setOnClickListener {
+            performRegister()
+        }
+
+
+
+    }
+
+    private fun performRegister(){
+        val email : EditText = findViewById(R.id.mail)
+        val password : EditText = findViewById(R.id.password)
+
+        val string1 = email.text.toString()
+        val string2 = password.text.toString()
+
+        if(string1.isEmpty() ||string2.isEmpty()){
+            Toast.makeText(this, "Please enter text in email/password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        Log.d("SignUp","Email is " + email)
+        Log.d("SignUp","Password :  $password")
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(string1, string2)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+                Log.d("SignUp", "Successfully created user : ${it.result?.user?.uid}")
+            }
+
+            .addOnFailureListener{
+                Log.d("SignUp", "Ailed to create user: ${it.message}")
+
+            }
 
     }
 }
