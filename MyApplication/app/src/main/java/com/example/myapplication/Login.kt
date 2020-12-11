a104791795dd3db27d9c07d14d045313d23af212
Nu Uyen Thi PHAN <thiphan94@gmail.com>
2020-12-11 10:52:18 +0100

    firebase login

--- MyApplication/app/src/main/java/com/example/myapplication/Login.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Login.kt
@@ -3,7 +3,11 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +24,31 @@ class Login : AppCompatActivity() {
         //go to gestion page
         val button2: Button = findViewById(R.id.log)
         button2.setOnClickListener {
-            val intent = Intent(this@Login, First::class.java)
-            startActivity(intent)
+            val email : EditText = findViewById(R.id.mail)
+            val password : EditText = findViewById(R.id.password)
+
+            val string1 = email.text.toString()
+            val string2 = password.text.toString()
+
+            Log.d("Login","Attempt login with email/pw: $email/***")
+
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(string1,string2)
+                .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success
+                    Log.d("Login", "signInWithEmail:success")
+                    val intent = Intent(this@Login, First::class.java)
+                    startActivity(intent)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("Login", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
         }
 
     }
