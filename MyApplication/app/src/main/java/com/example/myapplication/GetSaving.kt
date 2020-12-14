cc47f29ec49236a152682098dfa31eb41e82e69c
Nu Uyen Thi PHAN <thiphan94@gmail.com>
2020-12-14 18:41:30 +0100

    read saving data

--- MyApplication/app/src/main/java/com/example/myapplication/GetSaving.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/GetSaving.kt
@@ -1,11 +1,50 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.FirebaseFirestore
+import java.util.*
 
 class GetSaving : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_saving)
+
+
+        val db = FirebaseFirestore.getInstance()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        db.collection("saving")
+            .get()
+            .addOnCompleteListener { task ->
+                var list = ArrayList<SavingItem>()
+                if (task.isSuccessful) {
+                    for (document in task.result!!) {
+                        list.add(
+                            SavingItem(
+                                document.get("date") as String,
+                                document.get("amount") as Double
+                            )
+                        )
+                    }
+                    var adapter =SavingAdapter(list)
+                    recyclerView.adapter = adapter
+                } else {
+                    Log.w(ContentValues.TAG, "Error getting documents.", task.exception)
+                }
+            }
+
+
+
+
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/SavingAdapter.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/SavingAdapter.kt
@@ -3,25 +3,28 @@ package com.example.myapplication
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.core.view.doOnAttach
 import androidx.recyclerview.widget.RecyclerView
-import kotlin.collections.ArrayList
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import java.text.FieldPosition
 
-class DataAdapter(var list: ArrayList<DataItem>) : RecyclerView.Adapter<DataAdapter.ViewHolder>() {
+class SavingAdapter(var list: ArrayList<SavingItem>) : RecyclerView.Adapter<SavingAdapter.ViewHolder>() {
 
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
 
-        var type  = itemView.findViewById(R.id.type) as TextView
         var date = itemView.findViewById(R.id.date) as TextView
         var amount = itemView.findViewById(R.id.amount) as TextView
-        var category = itemView.findViewById(R.id.category) as TextView
+
     }
 
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent,false))
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.saving_layout, parent,false))
     }
 
     override fun getItemCount(): Int {
@@ -29,10 +32,9 @@ class DataAdapter(var list: ArrayList<DataItem>) : RecyclerView.Adapter<DataAdap
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.type.text = list[position].type
         holder.date.text = list[position].date
         holder.amount.text = list[position].amount.toString()
-        holder.category.text = list[position].category
+
 
     }
 
